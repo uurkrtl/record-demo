9a2f41a01beaad06c5cdc8e5b1de95cac47da012
ugurkartal <kartalugur@gmail.com>
2024-02-05 10:52:24 +0100

    create project

---
+++ src/main/java/net/ugurkartal/Animal.java
@@ -0,0 +1,9 @@
+package net.ugurkartal;
+
+public record Animal(
+        int id,
+        String name,
+        String species,
+        int age
+) {
+}
---
+++ src/main/java/net/ugurkartal/Main.java
@@ -0,0 +1,8 @@
+package net.ugurkartal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
